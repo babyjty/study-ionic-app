856f248a2f41d6220e61ef06b0ea6a58dd55fd67
DrCloudy <rmriaz444@gmail.com>
2022-03-11 18:15:13 +0800

    Update places.js

--- test-server/controllers/places.js
+++ test-server/controllers/places.js
@@ -10,7 +10,7 @@ const getLocations = (req, res) => {
 
     axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=1.356220%2C103.691551&key=AIzaSyA-1vQYn7cQBGGdn_7OkV4Lh073cXbQIdY&radius='+radius+'&type=cafe')
     .then(resp => {
-        // console.log(resp.data);
+        console.log(resp.data);
         return res.status(200).json(resp.data.results);
     })
     .catch(err => {
