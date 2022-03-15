8a70d994c95091bc7ea413c889cc0bfe263d8e9f
s207017 <64325566+s207017@users.noreply.github.com>
2022-03-15 22:17:19 +0800

    update

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,10 +8,6 @@ mainRouter.use('/api/jio', require('./jioRoutes/jioController'))
 mainRouter.use('/api/google', require('./userRoutes/googleController'))
 
 
-mongoose.connect(
-    config.mongoURI, {})
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err))
 
 mainRouter.get('/test', (req, res) => {
     res.send('Test dumb')
