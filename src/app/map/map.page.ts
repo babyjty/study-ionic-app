755872d7fdc1e239119d792aa896a9ca075eb7d7
DrCloudy <rmriaz444@gmail.com>
2022-04-06 14:23:10 +0800

    Update map.page.ts

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -29,7 +29,6 @@ export class MapPage {
   private sg_lat = 1.3521
   private sg_lng = 103.8198
   public profile: any;
-  public locationData;
   // constructor(public api:GooglePlacesAPIService){}
 
   constructor(public api: GooglePlacesAPIService, private geolocation: Geolocation, private profileApiService: ProfileApiService) { }
@@ -63,15 +62,15 @@ export class MapPage {
   getLocationsNearUser() {
     let loc = String(this.currentLoc[0].latitude) + '%2C' + String(this.currentLoc[0].longitude);
     this.api.getLocations(this.radius, loc).subscribe(result => {
-      this.locationData = result;
+      let locationData = result;
       let newLocation: object;
-      for (let key in this.locationData) {
-        if (this.locationData[key].rating && this.locationData[key].photos){
+      for (let key in locationData) {
+        if (locationData[key].rating && locationData[key].photos){
           newLocation = {
-          title: this.locationData[key].name,
-          latitude: String(this.locationData[key].geometry.location.lat),
-          longitude: String(this.locationData[key].geometry.location.lng),
-          rating: this.locationData[key].rating,
+          title: locationData[key].name,
+          latitude: String(locationData[key].geometry.location.lat),
+          longitude: String(locationData[key].geometry.location.lng),
+          rating: locationData[key].rating,
           }
           this.markers.push(newLocation);
         }
